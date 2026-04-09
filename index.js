4b99639e1062ab1a08d042276173d73f91555ebf
lishy322 <lishyemek@gmail.com>
2026-04-10 00:44:44 +0300

    Update index.js

--- index.js
+++ index.js
@@ -95,9 +95,25 @@ app.listen(PORT, () => {
 cron.schedule('* * * * *', async () => {
   console.log('cron עובד - בדיקה כל דקה');
 
+  const now = new Date();
+
   const snapshot = await db.collection('appointments').get();
 
-  snapshot.forEach(doc => {
-    console.log('תור:', doc.data());
+  snapshot.forEach(async (doc) => {
+    const data = doc.data();
+
+    const createdAt = new Date(data.createdAt);
+
+    const diffSeconds = (now - createdAt) / 1000;
+
+    // בדיקה: אם עברו יותר מ-60 שניות
+    if (diffSeconds > 60 && !data.reminded) {
+      console.log('צריך לשלוח תזכורת ל:', data.user);
+
+      // סימון כדי שלא ישלח פעמיים
+      await db.collection('appointments').doc(doc.id).update({
+        reminded: true
+      });
+    }
   });
 });
