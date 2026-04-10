5ed1ee52e9c794209340afcc0e7e71f633e0d577
lishy322 <lishyemek@gmail.com>
2026-04-11 00:36:37 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+const OpenAI = require('openai');
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
 const express = require('express');
 const admin = require('firebase-admin');
 const twilio = require('twilio');
