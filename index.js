9dc3dfb70ba9b5c8aaada81a7a1901f4a7aa2319
lishy322 <lishyemek@gmail.com>
2026-04-11 01:08:28 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const admin = require('firebase-admin');
+const serviceAccount = JSON.parse(process.env.FIREBASE_KEY);
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+const db = admin.firestore();;
 const twilio = require('twilio');
 const { MessagingResponse } = require('twilio').twiml;
 const OpenAI = require('openai');
