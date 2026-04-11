e47f7fa7bdd81756478884be2e44832310788b08
lishy322 <lishyemek@gmail.com>
2026-04-11 22:16:21 +0300

    Update index.js

--- index.js
+++ index.js
@@ -16,11 +16,7 @@ const twilio = require("twilio");
 const admin = require("firebase-admin");
 
 // ================= FIREBASE =================
-const serviceAccount = JSON.parse(process.env.FIREBASE_KEY);
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
 
 const db = admin.firestore();
 
